811e574a8efef3c5e4b078b514251385f8edc5d5
Samuel Kirton <samkirton@me.com>
2018-10-09 07:32:00 +0100

    Added the travis yml file

--- eos-core/src/main/kotlin/com/memtrip/eos/core/block/BlockIdDetails.kt
+++ eos-core/src/main/kotlin/com/memtrip/eos/core/block/BlockIdDetails.kt
@@ -24,6 +24,6 @@ import java.math.BigInteger
 data class BlockIdDetails(
     val blockId: String,
     val hexWriter: HexWriter = DefaultHexWriter(),
-    val blockNum: Int = BigInteger( 1, hexWriter.hexToBytes(blockId.substring(0, 8))).toInt(),
+    val blockNum: Int = BigInteger(1, hexWriter.hexToBytes(blockId.substring(0, 8))).toInt(),
     val blockPrefix: Long = Utils.readUint32(hexWriter.hexToBytes(blockId.substring(16, 24)), 0)
 )
